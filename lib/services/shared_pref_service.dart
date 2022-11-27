535437072bdef35600ac3f5a9d370eabed3fe1b4
Sinan Alagöz <sinanalagoz74@gmail.com>
2022-11-27 19:40:00 +0300

    555

--- lib/services/shared_pref_service.dart
+++ lib/services/shared_pref_service.dart
@@ -1,22 +1,41 @@
-import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:storage/const.dart';
 import 'package:storage/models/user_info.dart';
 
 class SharedPreferencService {
   saveDatas(UserInfo userInfo) async {
-    final preferences = await SharedPreferences.getInstance();
-    preferences.setString("name", userInfo.name);
-    preferences.setInt("gender", userInfo.gender);
-    preferences.setStringList("color", userInfo.colors);
-    preferences.setBool("isStudent", userInfo.isStudent);
+    final flutterSecureStorage = FlutterSecureStorage();
+    await flutterSecureStorage.write(key: "name", value: userInfo.name);
+    await flutterSecureStorage.write(
+        key: "gender", value: userInfo.gender.toString());
+    await flutterSecureStorage.write(
+        key: "color", value: jsonEncode(userInfo.colors));
+    await flutterSecureStorage.write(
+        key: "isStudent", value: userInfo.isStudent.toString());
+
+    
+    
   }
 
   Future<UserInfo> getDatas() async {
-    final preferences = await SharedPreferences.getInstance();
-    final name = preferences.getString("name") ?? "";
-    final gender = Gender.values[preferences.getInt("gender") ?? 0];
-    final colors = preferences.getStringList("color") ?? <String>[];
-    final isStudent = preferences.getBool("isStudent") ?? false;
+    final preferences = FlutterSecureStorage();
+
+    final name = await preferences.read(key: "name") ?? "";
+    final genderIndex = int.parse(await preferences.read(key: "gender") ?? "0");
+    final gender = Gender.values[genderIndex];
+    final colorString = await preferences.read(key: "color") ?? "[]";
+    final colors = List<String>.from(jsonDecode(colorString));
+
+    final iStudentString = await preferences.read(key: "isStudent") ?? "false";
+    final isStudent = iStudentString.toLowerCase()  == "true" ? true : false;
+
+    
+    
+    
+    
 
     return UserInfo(
         name: name, gender: gender.index, colors: colors, isStudent: isStudent);
